e99b6280ed01e10d46ee6e1c56392b9f2f581909
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-09 03:48:43 +0200

    added Navbar component to root layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+        <nav className="flex justify-between items-center">
+            <Link href="/">
+                <Image src="/logo.png" alt="link-a-thon logo" width={144} height={30} />
+            </Link>
+        </nav>
+    </header>
   )
 }
 
