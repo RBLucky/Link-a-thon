c99fda9eb3136e6a12e39de0e000e146ba12c46f
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-09 20:01:04 +0200

    refactored Header component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,9 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <>
-      <section className="tertiary_container">
-        <h1 className="heading">Pitch Your Project, <br /> Connect With Talent</h1>
-
-        <p className="sub-heading !max-w-3xl"></p>
-      </section>
+      <Header />
     </>
   );
 }
---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <section className="tertiary_container">
+        <h1 className="heading">Pitch Your Project, <br /> Connect With Talent</h1>
+
+        <p className="sub-heading !max-w-3xl">Showcase Ideas, Join a Project, and Get Noticed in Hackathons</p>
+      </section>
+    </>
+  )
+}
+
+export default Header;
