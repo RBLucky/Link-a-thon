06938d5511fdcef3b02f1c7b13d61293b434990c
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-11 01:18:32 +0200

    updated Hero section text

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,7 +26,7 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
       <section className="tertiary_container">
         <h1 className="heading">Pitch Your Project, <br /> Connect With Talent</h1>
 
-        <p className="sub-heading !max-w-3xl">Showcase Ideas, Join a Project, and Get Noticed in Hackathons</p>
+        <p className="sub-heading !max-w-3xl">Showcase Ideas, Collaborate on Projects, and Get Noticed in Hackathons</p>
 
         <SearchForm query={query} />
       </section>
