570000af03c716e23d478ef23aeaa24b551e5f9b
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-11 00:22:55 +0200

    imported project schema type in index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { author } from './author'
+import { project } from './project'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author],
+  types: [author, project],
 }
