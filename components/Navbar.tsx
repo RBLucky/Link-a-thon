dae380d9e3c82ccf83d23d96851edd1f85c9004a
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-19 11:20:17 +0200

    changed create button colour

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = async () => {
                     <>
                       <Link href="/project/create">
                         <span className="max-sm:hidden">Create</span>
-                        <BadgePlus className="size-6 mt-[-6px] sm:hidden" />
+                        <BadgePlus className="size-6 mt-[-6px] sm:hidden text-tertiary" />
                       </Link>
 
                       <form action={ async() => {
