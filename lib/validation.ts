a2d7f3564484d3e1b709c7d77b5652d6cc2400c8
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-19 02:40:23 +0200

    made validation more user-friendly

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    title: z.string().min(3).max(100),
-    description: z.string().min(20).max(500),
-    category: z.string().min(3).max(20),
-    link: z.string().url().refine(async (url) => {
+    title: z.string().min(3, "Your title must contain at least 3 characters.").max(100, "Your title must not exceed 100 characters."),
+    description: z.string().min(20, "Your description must contain at least 20 characters.").max(500, "Your description must not exceed 500 characters."),
+    category: z.string().min(3, "Your category must contain at least 3 characters.").max(20, "Your category must not exceed 20 characters."),
+    link: z.string().url("Please provide a valid URL.").refine(async (url) => {
         try {
             const res = await fetch(url, { method: "HEAD" });
             const contentType = res.headers.get("content-type");
@@ -14,6 +14,6 @@ export const formSchema = z.object({
         catch {
             return false;
         }
-    }),
-    pitch: z.string().min(10),
+    }, { message: "Please provide a URL for a valid image." }),
+    pitch: z.string().min(10, "Your pitch must contain at least 10 characters."),
 });
