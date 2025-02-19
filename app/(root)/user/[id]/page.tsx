d1035bc812754c6abbcaeb17b3aef084bb69d13a
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-19 09:32:46 +0200

    built user profile UI

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from "@/auth";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+
+export const experimental_ppr = true;
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
@@ -12,7 +15,46 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   if (!user) return notFound();
 
   return (
-    <div>page</div>
+    <>
+        <section className="profile_container">
+            <div className="profile_card">
+                <div className="profile_title">
+                    <h3 className="text-24-black uppercase text-center line-clamp-1">
+                        {user.name}
+                    </h3>
+                </div>
+
+                <Image
+                    src={user.image}
+                    alt={`${user.name}'s display picture`}
+                    width={220}
+                    height={220}
+                    className="profile_image"
+                />
+
+                <p className="text-30-extrabold mt-7 text-center">
+                    @{user?.username}
+                </p>
+
+                <p className="mt-1 text-center text-14 normal">
+                    {user?.bio}
+                </p>
+
+            </div>
+
+            <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+                <p className="text-30-bold">
+                    {session?.id == id ? "Your" : "All"} Projects
+                </p>
+
+                <ul className="card_grid-sm">
+                    {/* TODO: USER PROJECTS */}
+                </ul>
+
+            </div>
+
+        </section>
+    </>
   )
 }
 
