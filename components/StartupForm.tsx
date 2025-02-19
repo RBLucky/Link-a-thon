d72ba04c9513aa9b0ee404e1fa275813831bc6a5
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-19 05:16:16 +0200

    setup form submitting

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { Input } from "./ui/input";
-import { Textarea } from "./ui/textarea";
+import React, { useState, useActionState } from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import MDEditor from "@uiw/react-md-editor";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import { Send } from "lucide-react";
 import { formSchema } from "@/lib/validation";
+import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
 import { createPitch } from "@/lib/actions";
 
 const ProjectForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
-  const [pitch, setPitch] = useState<string>("This project is amazing because it...");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
+  const [pitch, setPitch] = useState("");
   const { toast } = useToast();
   const router = useRouter();
 
@@ -41,41 +40,34 @@ const ProjectForm = () => {
     "e.g StackOverflow, but everyone is nice",
     "e.g Takealot, but appropriate box sizes",
     "e.g Uber Walk",
-  ];
-
+    ];
+  
   const [placeholder, setPlaceholder] = useState<string>("e.g ");
-
+  
   useEffect(() => {
     setPlaceholder(placeholderText[Math.floor(Math.random() * placeholderText.length)]);
   }, []);
 
-  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setIsSubmitting(true);
-    setErrors({});
-
-    const formData = new FormData(event.currentTarget);
-    const formValues = {
-      title: formData.get("title") as string,
-      description: formData.get("description") as string,
-      category: formData.get("category") as string,
-      link: formData.get("link") as string,
-      pitch,
-    };
-
+  const handleFormSubmit = async (prevState: any, formData: FormData) => {
     try {
+      const formValues = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        category: formData.get("category") as string,
+        link: formData.get("link") as string,
+        pitch,
+      };
+
       await formSchema.parseAsync(formValues);
 
-      console.log("Form submitted successfully:", formValues);
+      const result = await createPitch(prevState, formData, pitch);
 
-      const result = await createPitch(formData, pitch);
-
-      if (result.status === "SUCCESS") {
+      if (result.status == "SUCCESS") {
         toast({
           title: "Success",
           description: "Your project pitch is ready for the world to see!",
         });
-  
+
         router.push(`/project/${result._id}`);
       } else {
         toast({
@@ -86,36 +78,61 @@ const ProjectForm = () => {
       }
 
       return result;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErorrs = error.flatten().fieldErrors;
 
-    } catch (error: any) {
-      const formattedErrors: Record<string, string> = {};
+        setErrors(fieldErorrs as unknown as Record<string, string>);
 
-      for (const [key, messages] of Object.entries(error.formErrors?.fieldErrors || {})) {
-        formattedErrors[key] = Array.isArray(messages) ? messages[0] : messages;
+        toast({
+          title: "Error",
+          description: "Please check your inputs and try again",
+          variant: "destructive",
+        });
+
+        return { ...prevState, error: "Validation failed", status: "ERROR" };
       }
-
-      setErrors(formattedErrors);
 
       toast({
         title: "Error",
-        description: "Please check your inputs and try again.",
+        description: "An unexpected error has occurred",
         variant: "destructive",
-      })
-    } finally {
-      setIsSubmitting(false);
+      });
+
+      return {
+        ...prevState,
+        error: "An unexpected error has occurred",
+        status: "ERROR",
+      };
     }
   };
 
+  const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+    error: "",
+    status: "INITIAL",
+  });
+
   return (
-    <form onSubmit={handleFormSubmit} className="project-form">
+    <form action={formAction} className="project-form">
       <div>
-        <label htmlFor="title" className="project-form_label">Title</label>
-        <Input id="title" name="title" className="project-form_input" required placeholder={placeholder} />
+        <label htmlFor="title" className="project-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="project-form_input"
+          required
+          placeholder={placeholder}
+        />
+
         {errors.title && <p className="project-form_error">{errors.title}</p>}
       </div>
 
       <div>
-        <label htmlFor="description" className="project-form_label">Description</label>
+        <label htmlFor="description" className="project-form_label">
+          Description
+        </label>
         <Textarea
           id="description"
           name="description"
@@ -123,11 +140,16 @@ const ProjectForm = () => {
           required
           placeholder="What It Do? (Briefly Explain Your Project)"
         />
-        {errors.description && <p className="project-form_error">{errors.description}</p>}
+
+        {errors.description && (
+          <p className="project-form_error">{errors.description}</p>
+        )}
       </div>
 
       <div>
-        <label htmlFor="category" className="project-form_label">Category</label>
+        <label htmlFor="category" className="project-form_label">
+          Category
+        </label>
         <Input
           id="category"
           name="category"
@@ -135,11 +157,16 @@ const ProjectForm = () => {
           required
           placeholder="Choose a Category (e.g Health, Education, Transport)"
         />
-        {errors.category && <p className="project-form_error">{errors.category}</p>}
+
+        {errors.category && (
+          <p className="project-form_error">{errors.category}</p>
+        )}
       </div>
 
       <div>
-        <label htmlFor="link" className="project-form_label">Image URL</label>
+        <label htmlFor="link" className="project-form_label">
+          Image URL
+        </label>
         <Input
           id="link"
           name="link"
@@ -147,11 +174,15 @@ const ProjectForm = () => {
           required
           placeholder="Paste a Link to Your Demo Or Some Media"
         />
+
         {errors.link && <p className="project-form_error">{errors.link}</p>}
       </div>
 
       <div data-color-mode="light">
-        <label htmlFor="pitch" className="project-form_label">Pitch</label>
+        <label htmlFor="pitch" className="project-form_label">
+          Pitch
+        </label>
+
         <MDEditor
           value={pitch || ""}
           onChange={(value) => setPitch(value as string)}
@@ -160,18 +191,24 @@ const ProjectForm = () => {
           height={300}
           style={{ borderRadius: 20, overflow: "hidden" }}
           textareaProps={{
-            placeholder: "Briefly describe your project and what your team needs...",
+            placeholder:
+              "Briefly describe your project and what your team needs...",
           }}
           previewOptions={{
             disallowedElements: ["style"],
           }}
           className="project-form_editor"
         />
+
         {errors.pitch && <p className="project-form_error">{errors.pitch}</p>}
       </div>
 
-      <Button type="submit" className="project-form_btn text-white" disabled={isSubmitting}>
-        {isSubmitting ? "Unleashing Your Brilliance..." : "Submit Your Pitch"}
+      <Button
+        type="submit"
+        className="project-form_btn text-white"
+        disabled={isPending}
+      >
+        {isPending ? "Unleashing Your Brilliance..." : "Submit Your Pitch"}
         <Send className="size-6 ml-2" />
       </Button>
     </form>
