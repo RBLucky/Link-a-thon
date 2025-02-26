7757ca6b5cc98b5a18cd552c15e9794ac6197231
RBLucky <nkosi.s.lucky@gmail.com>
2025-02-26 13:56:31 +0200

    enabled partial pre-rendering on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,8 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
 import { unstable_noStore } from "next/cache";
 
+export const experimental_ppr = true;
+
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }>}) {
 
   unstable_noStore();
